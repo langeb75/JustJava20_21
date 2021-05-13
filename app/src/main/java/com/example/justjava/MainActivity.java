bcde9b484254cfa81a8fa24d626a7f71a08c95e2
Brad Lange <blange@gulllakecs.org>
2021-05-13 10:42:58 -0400

    4.9 Update variable names for the EditText XML

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -16,6 +16,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 import java.text.NumberFormat;
 
@@ -57,6 +58,11 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
+        // Get text from the EditText XML
+        EditText nameField = (EditText) findViewById(R.id.name_field);
+        String name = nameField.getText().toString();
+        Log.v("MainActivity", "Name: " + name);
+
         // Figure out if the user wants whipped cream topping
         CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
         boolean hasWhippedCream =  whippedCreamCheckBox.isChecked();
